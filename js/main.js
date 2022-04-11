4da62d3036cc8ecf1b523d00bb9cfbb6f372a9df
TatianaKan <klanukt@gmail.com>
2022-04-11 22:30:49 +0300

    modal

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const modal = document.querySelector('.modal');
+const bottomsPlay = document.querySelectorAll('.button-play__wrap');
+const closeModal = document.querySelector('.close');
+const closeStopVideo = document.querySelector('iframe');
+
+console.log(closeStopVideo);
+
+bottomsPlay.forEach((item, i) => {
+  item.addEventListener('click', () => {
+    modal.classList.add('active');
+  })
+});
+
+modal.addEventListener('click', () => {
+  modal.classList.remove('active');
+  stopVideo();
+})
+
+const stopVideo = () => {
+  closeStopVideo.src = '';
+  closeStopVideo.src = 'https://www.youtube.com/embed/tZeMfF45Gmc';
+}
